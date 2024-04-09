08dce1d7ce7541caab34805065b861b1fa1c3c13
Pablo-west <edemkng4@gmail.com>
2024-04-09 00:27:58 +0000

    task 1:check-mate

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
-// 1-block-scoped.js
 export default function taskBlock(trueOrFalse) {
-  let task = false; // changed var to let
-  let task2 = true; // changed var to let
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    task = true; // no need to declare again, just assign
-    task2 = false; // no need to declare again, just assign
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
