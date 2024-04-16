30e50a8bbd193a81c13beddbfbc9f76f3d2004ed
Pablo-west <edemkng4@gmail.com>
2024-04-16 23:23:07 +0000

    update: 02

--- 0x03-ES6_data_manipulation/6-set.js
+++
@@ -1,7 +0,0 @@
-/**
- * Author: Pablo West
- * 6-set.js
- */
-export default function setFromArray(array) {
-  return new Set(array);
-}
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++
@@ -1,7 +0,0 @@
-/**
- * Author: Pablo West
- * 7-has_array_values.js
- */
-export default function hasValuesFromArray(set, array) {
-  return array.every((element) => set.has(element));
-}
